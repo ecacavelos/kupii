4ce607b601efe860790153cdeb1116a0380dded7
dayala <dayala@localhost>
2013-02-12 15:45:49 -0300

    - Se implent? el prototipo del m?dulo de clientes: Listado de Clientes, Consultar/Editar Cliente, Agregar Cliente, Borrar Cliente. TODO: Busqueda personalizada de registro de clientes.

--- clientes/urls.py
+++ clientes/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('clientes.views',    
-    url(r'^$', 'clientes'),
-    url(r'^consultar/$', 'consultar_clientes'),
+    url(r'^$', 'clientes'),    
+    url(r'^listado/$', 'consultar_clientes'),
+    url(r'^listado/(?P<cliente_id>\d+)/$', 'detalle_cliente'),
     url(r'^agregar/$', 'agregar_clientes'),
+    url(r'^borrar/(?P<cliente_id>\d+)/$', 'borrar_cliente'),
 )
--- clientes/views.py
+++ clientes/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.shortcuts import render_to_response
 from django.template import RequestContext, loader
 from datos.models import Cliente
 from clientes.forms import ClienteForm
@@ -9,7 +10,7 @@ def clientes(request):
     c = RequestContext(request, {})
     return HttpResponse(t.render(c))
 
-#vista para listar todos los clientes
+#vista para consultar el listado de todos los clientes
 def consultar_clientes(request):
     t = loader.get_template('clientes/consultar.html')
     object_list = Cliente.objects.all()
@@ -18,6 +19,23 @@ def consultar_clientes(request):
     })
     return HttpResponse(t.render(c))
 
+#vista para consultar el detalle de un cliente
+def detalle_cliente(request, cliente_id):
+    t = loader.get_template('clientes/detalle.html')
+    object_list = Cliente.objects.get(pk=cliente_id)
+    form = ClienteForm(instance=object_list)
+    #form.save()
+    c = RequestContext(request, {
+        'cliente': object_list,
+        'form': form,
+    })
+    return HttpResponse(t.render(c))
+#    try:
+#        c = Cliente.objects.get(pk=cliente_id)
+#    except Cliente.DoesNotExist:
+#        raise Http404
+#    return render_to_response('clientes/detalle.html', {'cliente': c})
+
 # vista para agregar un nuevo cliente
 def agregar_clientes(request):
     t = loader.get_template('clientes/agregar.html')
@@ -25,7 +43,7 @@ def agregar_clientes(request):
         form = ClienteForm(request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect('/clientes/consultar')
+            return HttpResponseRedirect('/clientes/listado')
     else:
         form = ClienteForm()        
         
@@ -33,3 +51,8 @@ def agregar_clientes(request):
         'form': form,
     })
     return HttpResponse(t.render(c))
+
+def borrar_cliente(request, cliente_id):
+    c = Cliente.objects.get(pk=cliente_id)
+    c.delete()
+    return HttpResponseRedirect('/clientes/listado')
