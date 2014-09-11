228e286948703fc40cd6069db0be871021e81132
jdanilo <jose.barone@cbi.com.py>
2014-09-11 12:03:43 -0400

    Se incluye el archivo del modulo monthdelta

--- principal/common_functions.py
+++ principal/common_functions.py
@@ -1,6 +1,7 @@
 from django.db.models import Count, Min, Sum, Avg
 from principal.models import Lote, Cliente, Vendedor, PlanDePago, Fraccion, Manzana, Venta, Propietario, PlanDePagoVendedor,PagoDeCuotas
-from monthdelta import MonthDelta
+from principal.monthdelta import MonthDelta 
+
 
 def get_cuotas_detail_by_lote(lote_id=None):
 
@@ -9,7 +10,7 @@ def get_cuotas_detail_by_lote(lote_id=None):
     cant_cuotas_pagadas=PagoDeCuotas.objects.filter(lote=lote_id).aggregate(Sum('nro_cuotas_a_pagar'))
     venta = Venta.objects.get(lote_id=lote_id)
     plan_de_pago = PlanDePago.objects.get(id=venta.plan_de_pago.id)
-#     calcular la fecha de vencimiento.
+    # calcular la fecha de vencimiento.
     proximo_vencimiento = (venta.fecha_de_venta + MonthDelta(cant_cuotas_pagadas['nro_cuotas_a_pagar__sum'])).strftime('%d/%m/%Y')
     datos = dict([('cant_cuotas_pagadas', cant_cuotas_pagadas['nro_cuotas_a_pagar__sum']), ('cantidad_total_cuotas', plan_de_pago.cantidad_de_cuotas), ('proximo_vencimiento', proximo_vencimiento)])
     return datos    
