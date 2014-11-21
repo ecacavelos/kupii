c7857c03e05ebfeab0bb1d91a7ed46b38e57c3b5
Esteban Cacavelos <estebancacavelos@gmail.com>
2014-11-21 17:49:54 -0300

    correcciones con respecto al registro de las apps fracciones y clientes de django

--- principal/admin.py
+++ principal/admin.py
@@ -4,13 +4,12 @@ from principal.models import Cliente
 class ClienteAdmin(admin.ModelAdmin):
     list_display = ['__unicode__', 'cedula']
     search_fields = ['nombres', 'apellidos']
-admin.site.register(Cliente, ClienteAdmin)
-
+# admin.site.register(Cliente, ClienteAdmin)
 # class LoteAdmin(admin.ModelAdmin):
 #     list_display = ['__unicode__', 'estado']
 #     ordering = ['fraccion', 'manzana', 'nro_lote']
 # admin.site.register(Lote, LoteAdmin)
-admin.site.register(Fraccion)
+# admin.site.register(Fraccion)
 # admin.site.register(Manzana)
 # admin.site.register(Lote)
 # admin.site.register(Vendedor)
