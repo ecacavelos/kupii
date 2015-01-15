2deed3afc76e689399e003c4015899722a76f316
jdanilo <jose.barone@cbi.com.py>
2015-01-15 12:58:03 -0300

    INMOBIADM-165 Se agrega interfaz para listar las facturas, con buscador por fecha

--- facturas/urls.py
+++ facturas/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('facturas.views',        
     url(r'^facturar/$', 'facturar'),
+    url(r'^listado/(?P<factura_id>\d+)/$', 'detalle_factura'),
+    url(r'^listado/$', 'consultar_facturas'),
 )
--- facturas/views.py
+++ facturas/views.py
@@ -13,9 +13,8 @@ from django.db import connection
 import xlwt
 import math
 
-# Funcion principal del modulo de lotes.
-def facturar(request):
-    
+# Funcion principal del modulo de facturas.
+def facturar(request):    
     if request.user.is_authenticated():
         if request.method == 'GET':
             #Mostrar el formulario basico de factura.            
@@ -57,3 +56,76 @@ def facturar(request):
             return HttpResponse(t.render(c))
     else:
         return HttpResponseRedirect("/login") 
+    
+    
+# Funcion para consultar el listado de todas las facturas.
+def consultar_facturas(request):
+    if request.user.is_authenticated():
+        if request.method == 'GET':
+            #Mostrar el formulario basico de factura.  
+            object_list = Factura.objects.all().order_by('id')          
+            #c = RequestContext(request, {})
+            #return HttpResponse(t.render(c))
+        else: #POST se envia el formulario.  
+            data = request.POST     
+            #object_list = Factura.objects.all().order_by('id')
+            # En caso de que se haya solicitado una busqueda, filtramos de acuerdo al parametro correspondiente.
+            fecha_ini = data.get('fecha_ini', '')
+            fecha_fin = data.get('fecha_fin', '')
+            fecha_ini_parsed = str(datetime.strptime(fecha_ini, "%d/%m/%Y").date())
+            fecha_fin_parsed = str(datetime.strptime(fecha_fin, "%d/%m/%Y").date())
+            object_list = Factura.objects.filter(fecha__range=(fecha_ini_parsed,fecha_fin_parsed)).order_by('id','fecha')                    
+        
+        paginator=Paginator(object_list,15)
+        page=request.GET.get('page')
+        try:
+            lista=paginator.page(page)
+        except PageNotAnInteger:
+            lista=paginator.page(1)
+        except EmptyPage:
+            lista=paginator.page(paginator.num_pages)
+    
+        t = loader.get_template('facturas/listado.html')
+        c = RequestContext(request, {
+            'object_list': lista,
+            #'message': message,
+        })
+        return HttpResponse(t.render(c))
+    else:
+        return HttpResponseRedirect("/login") 
+   
+''' 
+# Funcion para consultar el detalle de una factura.
+def detalle_factura(request, factura_id):    
+    if request.user.is_authenticated():
+        t = loader.get_template('facturas/detalle.html')
+        #c = RequestContext(request, {})
+        #return HttpResponse(t.render(c))
+    else:
+        return HttpResponseRedirect("/login")
+
+    object_list = Cliente.objects.get(pk=cliente_id)
+    message = ''
+
+    if request.method == 'POST':
+        data = request.POST
+        if data.get('boton_guardar'):
+            form = ClienteForm(data, instance=object_list)
+            if form.is_valid():
+                message = "Se actualizaron los datos."
+                form.save(commit=False)
+                object_list.save()
+        elif data.get('boton_borrar'):
+            c = Cliente.objects.get(pk=cliente_id)
+            c.delete()
+            return HttpResponseRedirect('/clientes/listado')
+    else:
+        form = ClienteForm(instance=object_list)
+
+    c = RequestContext(request, {
+        'cliente': object_list,
+        'form': form,
+        'message': message,
+    })
+    return HttpResponse(t.render(c))
+'''
